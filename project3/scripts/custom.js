3eef9c2049bb34a242eba95eb6cf97189e8ea48a
kellerthorpe <KimberlyRose01@gmail.com>
2016-03-19 00:06:38 -0400

    Update
    Fixing changes by hand, since Github won't upload the file.

--- project3/scripts/custom.js
+++ project3/scripts/custom.js
@@ -16,16 +16,6 @@ $("form").submit(function(e) {
 });
 
 /*
-$(function(){           
-        if (!Modernizr.inputtypes.date) {
-        // If not native HTML5 support, fallback to jQuery datePicker
-            $('input[type=date]').datepicker({
-                // Consistent format with the HTML5 picker
-                    dateFormat : 'yy-mm-dd'
-                },
-                // Localization
-                $.datepicker.regional['it']
-            );
-        }
-    });
-    */
+Code from:
+http://stackoverflow.com/questions/23261301/required-attribute-not-work-in-safari-browser
+*/
